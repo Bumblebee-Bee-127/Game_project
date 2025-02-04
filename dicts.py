fb357c6d2fb3eed676c4495955edf6155f76c00a
Bumblebee-Bee-127 <slavaalimova@yandex.ru>
2025-02-05 01:48:10 +0300

    menu and levels

--- dicts.py
+++ dicts.py
@@ -1,7 +1,19 @@
 button_text = {1: 'Уровень 1', 2: 'Уровень 2', 3: 'Уровень 3',
                4: 'Уровень 4', 5: 'Уровень 5', 6: 'Уровень 6'}
 
-type_block_picture = {'land': {0: 'pictures/land/grass.png', 1: 'pictures/land/ground.png', 2: 'pictures/land/sand.png',
-                               3: 'pictures/land/stone-1.png', 4: 'pictures/land/stone-2.png'},
-                      'walls': {5: 'pictures/land/stone-3.png', 6: 'pictures/land/stone-4.png', 7: 'pictures/land/ground-1.png'},
-                      'wood': {8: 'pictures/land/wood-1.png', 9: 'pictures/land/wood-2.png'}}
+type_block_picture = {'land': {0: 'pictures/blocks/land/grass.png', 1: 'pictures/blocks/land/ground.png', 2: 'pictures/blocks/land/sand.png',
+                               3: 'pictures/blocks/land/stone-1.png', 4: 'pictures/blocks/land/stone-2.png'},
+
+                      'walls': {5: 'pictures/blocks/walls/stone-3.png', 6: 'pictures/blocks/walls/stone-4.png', 7: 'pictures/blocks/walls/ground-1.png'},
+
+                      'wood': {8: 'pictures/blocks/wood/wood-1.png', 9: 'pictures/blocks/wood/wood-2.png'}}
+
+type_plants_picture = {'flowers': {0: 'pictures/objects/plants/flowers/flower-1.png', 1: 'pictures/objects/plants/flowers/flower-2.png',
+                                   2: 'pictures/objects/plants/flowers/flower-3.png', 3: 'pictures/objects/plants/flowers/flower-4.png'},
+
+                      'grass': {5: 'pictures/objects/plants/grass/grass-1.png', 6: 'pictures/objects/plants/grass/grass-2.png',
+                                7: 'pictures/objects/plants/grass/grass-3.png', 8: 'pictures/objects/plants/grass/grass-4.png'},
+
+                      'trees': {'q': 'pictures/objects/plants/trees/tree-1.png', 'w': 'pictures/objects/plants/trees/tree-2.png',
+                                'e': 'pictures/objects/plants/trees/tree-3.png', 'r': 'pictures/objects/plants/trees/tree-4.png',
+                                't': 'pictures/objects/plants/trees/tree-5.png', 'y': 'pictures/objects/plants/trees/dark-tree.png'}}
---
+++ enemy.py
@@ -0,0 +1,5 @@
+import pygame
+
+import sys
+class Enemy():
+    def __init__(self, x, y, text, image, name, ckill = None):
